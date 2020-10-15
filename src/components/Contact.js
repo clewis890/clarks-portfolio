9f79c0dc37456e8681edabbe911c733cfb6f9688
clewis890 <60119523+clewis890@users.noreply.github.com>
2020-10-15 09:16:19 -0300

    changed contact email and added button

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -13,7 +13,7 @@ class Contact extends React.Component {
                         <div className="contact-text">
                             <h2>Get in touch!</h2>
                                 <p>I'm eager, enthusiastic, and hard working. I'm actively looking for my first position as a Web Developer, and am always looking for projects for my portfolio! Feel free to reach out if you need help building a site or even if you just have a question!
-                                <br /> Reach out via email or LinkedIn. I look forward to working with you! <br /><button className="btn"><a href="mailto:hello@clarkglewis.com" target="_blank" rel="noopener noreferrer">Contact Me!</a></button></p>
+                                <br /> Reach out via email or LinkedIn. I look forward to working with you! <br /><button className="btn"><a href="mailto:clewis890@gmail.com" target="_blank" rel="noopener noreferrer">Contact Me!</a></button></p>
                                 
                         </div>
                     </div>
